290ae6a58e44b1d4563b04060d8a7e65812e4f66
crousekimo <43980978+crousekimo@users.noreply.github.com>
2022-12-26 15:40:17 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -126,64 +126,138 @@ namespace microbiti2cesp32 {
     export function setanalogR(pin: analogRpin):number {
         return parseFloat(receivei2cmessage("analogRead="+pin.toString()))
     }   
-	
+	//% group="1.MQTT"  
     //% blockId=subMqtt block="Subscribe mqtt %topic"
     //% weight=97 
     export function subMqtt(topic: string):void {
          sendi2cmessage("sebmqtt="+topic)
     }
+		//% group="1.MQTT"  
     //% blockId=ReceiveMqttTopic block="receive mqtt topic"
     //% weight=97 	
     export function ReceiveMqttTopic():string {
         return receivei2cmessage("mqttrect=").substr(1)
     }  
+		//% group="1.MQTT"  
     //% blockId=ReceiveMqttMessage block="receive mqtt message"
     //% weight=97 	
     export function ReceiveMqttMessage():string {
         return receivei2cmessage("mqttrecm=").substr(1)
     }  
+		//% group="1.MQTT"  
     //% blockId=sendmqtt block="send mqtt topic %topic | message %message "
     //% weight=56 
     export function sendmqtt(topic: string, message: string):void {
         sendi2cmessage("sendmqtt="+topic+","+message)
     }  
+   //% group="2.Line notify"  
     //% blockId=linetoken block="Line notify token %token "
     //% weight=56 
     export function linetoken(token: string):void {
         sendi2cmessage("linetoken="+token)
     }  
-	
+	 //% group="2.Line notify"  
     //% blockId=linemessage block="Line notify message %message "
     //% weight=56 
     export function linemessage(message: string):void {
         sendi2cmessage("linemessage="+message)
     }  
-	
-	
-    //% blockId=openweathermapsetup block="OpenWeatherMap key %key "
-    //% weight=56 
-    export function openweathermapsetup(key: string):void {
-        sendi2cmessage("openweathermapsetup="+key)
-    }  
-	
-    //% blockId=openweathermapcity block="OpenWeatherMap city %city "
-    //% weight=56 
-    export function openweathermapcity(city: string):void {
-        sendi2cmessage("openweathermapcity="+city)
-    }  
-
-    //% blockId=openweathermapreturn block="OpenWeatherMap option %option "
-    //% weight=56 
-    export function openweathermapreturn(option: openweathermapmenu):number {
-        return parseFloat(receivei2cmessage("openweathermapreturn="+option.toString()).substr(1))
-    } 
-	
+    //% group="2.Line notify"  
     //% blockId=linesticker block="Line notify sticker message %message | packageID %packageID | stickerID %stickerID "
     //% weight=56 
     export function linesticker(message: string,packageID: number, stickerID: number):void {
         sendi2cmessage("linesticker="+message+","+packageID.toString()+","+stickerID.toString())
     }  
-			
+  //% group="2.OpenWeatherMap"  	
+    //% blockId=openweathermapsetup block="OpenWeatherMap key %key "
+    //% weight=56 
+    export function openweathermapsetup(key: string):void {
+        sendi2cmessage("openweathermapsetup="+key)
+    }  
+	  //% group="2.OpenWeatherMap"  
+    //% blockId=openweathermapcity block="OpenWeatherMap city %city "
+    //% weight=56 
+    export function openweathermapcity(city: string):void {
+        sendi2cmessage("openweathermapcity="+city)
+    }  
+  //% group="2.OpenWeatherMap"  
+    //% blockId=openweathermapreturn block="OpenWeatherMap option %option "
+    //% weight=56 
+    export function openweathermapreturn(option: openweathermapmenu):number {
+        return parseFloat(receivei2cmessage("openweathermapreturn="+option.toString()).substr(1))
+    } 
+
+	
+    //% group="3.Thingspeak"      
+    //% blockId=thingspeak1 block="Connect to Thingspeak key %key | Write Field1 value %value1 "
+    //% weight=101 
+    export function thingspeak1(key:string, value1: string):void {
+        check()
+        serial.writeLine("t\="+key+","+value1+"\\n")
+        basic.pause(4000)
+    }
+    //% group="3.Thingspeak"            
+    //% blockId=thingspeak4 
+    //% block="Connect to Thingspeak key %key | Write Fields value | Field1 value %value1 || Field2 value %value2 Field3 value %value3 Field4 value %value4 Field5 value %value5 Field6 value %value6 Field7 value %value7"
+    //% weight=101  
+    //% blockExternalInputs=1
+    export function thingspeak4(key:string, value1: number, value2?:number, value3?:number, value4?:number, value5?:number, value6?:number, value7?:number):void {    
+        check()
+        let b=""
+        let i
+        let value12:number[]=[value1,value2,value3,value4,value5,value6,value7]
+        for (i=0;i<7;i++)
+        {
+              if (i==0)
+              {
+                    b=value12[0].toString()
+              }else if (value12[i]!=null)
+              {
+                    let c=i+1
+                    b=b+"\&field"+c.toString()+"="+value12[i].toString()
+              }
+        }
+        serial.writeLine("t\="+key+","+b+"\\n")
+        basic.pause(8000)
+    }
+    //% group="3.Thingspeak"      
+    //% blockId=thingspeak2 block="Connect to Thingspeak key %key | Write Fields value %value1 "
+    //% weight=101
+    export function thingspeak2(key:string, value1: number[]):void {
+        check()
+        let a=value1.length
+        let b=""
+        let i
+        for (i=0;i<a;i++)
+        {
+              if (i==0)
+              {
+                    b=value1[0].toString()
+              }else
+              {
+                    let c=i+1
+                    b=b+"\&field"+c.toString()+"="+value1[i].toString()
+              }
+        }
+        serial.writeLine("t\="+key+","+b+"\\n")
+        basic.pause(8000)
+    }
+
+      
+     //% group="3.Thingspeak"  
+     //% blockId=thingspeak3 block="Connect to Thingspeak Channel ID %key | Read %value1 value"
+    //% weight=101
+    export function thingspeak3(key:number, value1: value555): number {
+        check()
+        serial.writeLine("tt="+convertToText(key)+","+convertToText(value1)+",1"+"\\n")
+        basic.pause(500)
+        let a=serial.readString()
+        basic.pause(500)
+        return parseFloat(a)
+
+    }     
+	
+	
     function sendi2cmessage(command: string):void {
         for (let index = 0; index <= command.length-1; index++) {
         	pins.i2cWriteNumber(
