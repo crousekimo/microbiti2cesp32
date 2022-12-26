b406757fea9a5fe368beae61e575b1e9837f3cc5
crousekimo <43980978+crousekimo@users.noreply.github.com>
2022-12-26 20:51:40 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,5 @@
 //% weight=0 color=#EC7505 icon="\uf0ad" block="microbiti2cesp32"
 namespace microbiti2cesp32 {
-      export enum analogRpin {
-        A32 = 32,
-        A33 = 33,
-        A34 = 34,
-        A35 = 35,
-        A36 = 36,
-	A39 = 39
-     }
-     
-      export enum digitalWpin {
-        D2 = 2,
-	D4 = 4,
-        D13 = 13,
-        D16 = 16,
-        D17 = 17,
-        D18 = 18,
-        D19 = 19,
-        D21 = 21,
-        D22 = 22,
-        D23 = 23,
-        D24 = 24,
-        D25 = 25,
-        D26 = 26,
-        D27 = 27,
-        D32 = 32,
-	D33 = 33
-     }
-      
-      export enum analogWpin {
-        A2 = 2,
-	A4 = 4,
-        A13 = 13,
-        A16 = 16,
-        A17 = 17,
-        A18 = 18,
-        A19 = 19,
-        A21 = 21,
-        A22 = 22,
-        A23 = 23,
-        A24 = 24,
-        A25 = 25,
-        A26 = 26,
-        A27 = 27,
-        A32 = 32,
-	A33 = 33
-     }
-
-	
-   export enum digitalRpin {
-        D2 = 2,
-        D13 = 13,
-        D16 = 16,
-        D17 = 17,
-        D18 = 18,
-        D19 = 19,
-        D21 = 21,
-        D22 = 22,
-        D23 = 23,
-        D24 = 24,
-        D25 = 25,
-        D26 = 26,
-        D27 = 27,
-        D32 = 32,
-	D33 = 33,
-        D36 = 36,
-	D39 = 39
-     }	
 
      export enum openweathermapmenu {
         Lon = 1,
@@ -76,17 +9,7 @@ namespace microbiti2cesp32 {
         Humidity = 5,
 	WindSpeed = 6
      }
-	
-	
-	
-      export enum type {
-        INPUT = 2,
-        OUTPUT = 1
-     }
-     export enum value {
-        HIGH = 1,
-        LOW = 0
-     }
+
      let mqttmessage="";
      let mqtttopic="";
      
@@ -100,38 +23,6 @@ namespace microbiti2cesp32 {
         field7 = 7,
         field8 = 8
      }
-    //% blockId=setpinmode block="set esp32 digital pin %pin | for %XY"
-    //% weight=101
-    export function setpinmode(pin: digitalWpin, XY: type):void {
-       sendi2cmessage("pinMode="+pin.toString()+","+XY.toString())    
-    }
-     
-     
-    //% blockId=setdigitalW block="set esp32 digital pin  %pin | value to %XY"
-    //% weight=100
-    export function setdigitalW(pin: digitalWpin, XY: value):void {
-        sendi2cmessage("digitalWrite="+pin.toString()+","+XY.toString())    
-    }
-    
-    //% blockId=setanalogW block="set esp32 digital pin  %pin | PWM value to %XY"
-    //% weight=99
-    export function setanalogW(pin: analogWpin, XY: number):void {
-        sendi2cmessage("analogWrite="+pin.toString()+","+XY.toString())    
-    }
-
-	
-    //% blockId=setdigitalR block="read esp32 digital pin  %pin value"
-    //% weight=98
-    export function setdigitalR(pin: digitalRpin):number {
-        return parseFloat(receivei2cmessage("digitalRead="+pin.toString()))
-    }
-
-    //% blockId=setanalogR block="read esp32 analog pin  %pin value"
-    //% weight=97 
-    export function setanalogR(pin: analogRpin):number {
-        return parseFloat(receivei2cmessage("analogRead="+pin.toString()))
-    }   
-	
     //% group="1.Setup"
     //% blockId=setWiFi block="Set ESP32 | SSID %SSID| Pass %PASS"
     //% weight=101
